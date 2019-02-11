a7ca474fbb4c64d43dba4483a2e2a82707eea58b
Sunil <sunil.manikonda@utoronto.ca>
2019-02-11 15:14:26 -0500

    Metrics broken by 4.10.1 upgrade, change in database schema

--- sql/get_dataset_name.sql
+++ sql/get_dataset_name.sql
@@ -1,5 +1,6 @@
-Select value,identifier, protocol from datasetfield 
+Select value, identifier, protocol from datasetfield
 join datasetfieldvalue on datasetfieldvalue.datasetfield_id=datasetfield.id
 join datasetversion on datasetfield.datasetversion_id = datasetversion.id
 join  dataset on dataset.id= dataset_id
+join  dvobject on dvobject.id= dataset_id
 where datasetversion.id in (select max(datasetversion.id) as max from datasetversion group by datasetversion.dataset_id) and "datasetfieldtype_id" = 1 and dataset_id= {id}
